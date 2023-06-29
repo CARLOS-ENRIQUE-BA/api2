85e5f716a85e7b0a4c78474cc041a7fb32a7c69a
carloS <CARLOS-ENRIQUE-BA>
2023-06-29 14:33:33 -0600

    segundo commit

---
+++ product/domain/Product.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(id, name, race) {
+        this.id = id;
+        this.name = name;
+        this.race = race;
+    }
+}
+exports.Product = Product;
